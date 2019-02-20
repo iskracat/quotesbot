c1537d9e5054d9c785d119f35980032a29d85ca4
Nil <n.bacardit@iskra.cat>
2019-02-20 18:55:18 +0100

    last changes

--- quotesbot/spiders/scrape_urls.py
+++ quotesbot/spiders/scrape_urls.py
@@ -4,19 +4,33 @@ import re
 from scrapy.selector import HtmlXPathSelector
 from scrapy.http import Request
 
-URL = "https://www.cablematic.es/"
+import time
 
-URL2 = "https://www.cablematic.es"
+all_domains = [
+    "https://www.cablematic.es",
+    "https://www.cablematic.fr",
+    "https://www.cablematic.it",
+    "https://www.cablematic.pt",
+    "https://www.cablematic.co.uk",
+    "https://www.cablematic.cat",
+    "https://www.cablematic.de",
+    "https://www.cablematic.ie",
+    "https://www.cablematic.be"
+]
+
+URL = "https://www.cablematic.cat/"
+
+URL2 = "https://www.cablematic.cat"
 
 pattern = re.compile(r".*/.*/[\w]{4}/$")
 
-pattern_page = re.compile(r".*/\?pag=[0-9]+$")
+pattern_page = re.compile(r".*/.*/.*\?pag=[0-9]+$")
 
-file_urls = open("./all_urls.txt", "w")
+file_urls = open("./all_urls.txt", "a+")
 all_urls = []
 #menucat_container > div
 
-urls = open("urls.txt", "r")
+urls = open("./urls.txt", "r")
 lines_url_newline = urls.readlines()
 urls.close()
 lines_url = []
@@ -24,6 +38,21 @@ lines_url = []
 for url in lines_url_newline:
     lines_url.append(url.strip('\n'))
 
+def compute(number):
+    length = len(lines_url)
+    if number == 1:
+        prefix = 0
+        sufix = int(length/5)
+    elif number == 5:
+        prefix = int(length/5) * (number-1)
+        sufix = None
+    else:
+        prefix = int(length/5) * (number-1)
+        sufix = int(length/5) * number
+    return prefix, sufix
+
+prefix, sufix = compute(5)
+    
 class MySpider(scrapy.Spider):
     name = 'scrape_urls'
     start_urls = [
@@ -32,31 +61,37 @@ class MySpider(scrapy.Spider):
     def get_products(self, response):
         hxs = HtmlXPathSelector(response)
         for url in hxs.select('//a/@href').extract():
-            if "producto" in url:
+            if "producte" in url:
                 result = pattern.search(url)
                 if result and result.group() not in all_urls:
+                    print(result.group(), len(all_urls))
                     all_urls.append(result.group())
                     file_urls.write(result.group() + '\n')
-                    print(result.group(), len(all_urls))
             elif 'pag' in url:
                 result = pattern_page.search(url)
                 if result:
                     if not ( url.startswith('http://') or url.startswith('https://') ):            
                         url= URL2 + url
                     yield Request(url, callback=self.get_products)
-    
+
+    def extract_categories(self, response):
+        hxs = response.xpath('//div[@id="menucat_container"]').select('//a/@href').extract()
+        return hxs
+
     def parse(self, response):
         try:
-            for url in lines_url:
-                if "producto" in url:
+            for url in lines_url[prefix:sufix]:
+                if "producte" in url:
                     result = pattern.search(url)
                     if result:
                         if result.group() not in all_urls:
+                            print(result.group())
                             all_urls.append(result.group())
                             file_urls.write(result.group() + '\n')
-                            print(result.group(), len(all_urls))
-                elif url in lines_url:
-                    url = URL + url
+                else:
+                    print(url)
+                    if not ( url.startswith('http://') or url.startswith('https://') ):
+                        url = URL + url
                     yield Request(url, callback=self.get_products)
         except scrapy.exceptions.NotSupported as e:
             pass
